8125638ac1d4d79304f07f7d67a430d37b155217
Chyngyz <ryskeldiev_ch@iuca.kg>
2023-06-22 04:22:31 +0600

    front_shedule_without_links

---
+++ frontend/schedule-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Schedule from './components/Schedule';
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Школа программирования</h1>
+      <Schedule />
+    </div>
+  );
+}
+
+export default App;
